8779e201fe878fa0e578b6dab2fd5b28562c6942
Ujjwal Kulkarni <uk2011@nyu.edu>
2022-10-19 12:59:33 -0400

    test

--- swe1app/settings.py
+++ swe1app/settings.py
@@ -23,10 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-#SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 SECRET_KEY = config('SECRET_KEY')
-#SECRET_KEY = "django-insecure-zzzy2eu5zi_kz6*euhbgy^k(mbn^tqj=jk$ke7w@c7^-8noz+g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
